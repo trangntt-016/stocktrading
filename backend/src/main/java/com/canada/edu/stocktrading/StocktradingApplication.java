51956072cc29bb4d947ad1a37067ad3bee0f4eb0
trangntt-016 <trangntt.016@gmail.com>
2021-06-30 02:03:14 -0400

    create backend folder

---
+++ backend/src/main/java/com/canada/edu/stocktrading/StocktradingApplication.java
@@ -0,0 +1,13 @@
+package com.canada.edu.stocktrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocktradingApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/canada/edu/stocktrading/StocktradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canada.edu.stocktrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
