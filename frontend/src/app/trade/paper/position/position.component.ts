8175d1708fe851120d5d1d4dd6967c71c3c2c490
trangntt-016 <trangntt.016@gmail.com>
2021-07-24 22:18:05 -0400

    position layout

---
+++ frontend/src/app/trade/paper/position/position.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-position',
+  templateUrl: './position.component.html',
+  styleUrls: ['./position.component.css']
+})
+export class PositionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
