761808b89637b05460c3c66414ce906276257c7f
trangntt-016 <trangntt.016@gmail.com>
2021-07-29 05:32:03 -0400

    showMatchedPrice in buysell.component.html, buysell.component.ts

--- frontend/src/app/trade/paper/buysell/buysell.component.ts
+++ frontend/src/app/trade/paper/buysell/buysell.component.ts
@@ -7,10 +7,7 @@ import { environment } from '../../../../environments/environment';
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 import { StockUtils } from '../../../utils/StockUtils';
-import { DailyDetails } from '../../../model/DailyDetails';
 import { DailyBidAsk } from '../../../model/DailyBidAsk';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -24,6 +21,8 @@ export class BuysellComponent implements OnInit {
   autoSymbols: Symbol[];
   searchSymbol: string;
   daily: DailyBidAsk;
+  matchedPrice: number;
+  showMatched: boolean;
   private serverUrl = environment.stockWS;
   private stompClient;
   private stompClientSub;
@@ -50,7 +49,9 @@ export class BuysellComponent implements OnInit {
     this.symbolService.selectedSymbolEvt.subscribe(symbol => {
       this.stompClient.subscribe(`/topic/trade/${symbol.symbolId}`, (daily) => {
         this.daily = this.utils.convertToDailyBidAsk(daily.body);
-        console.log(this.daily);
+      });
+      this.stompClient.subscribe(`/topic/trade/${symbol.symbolId}/future`, (matched) => {
+        this.matchedPrice = matched.body;
       });
       this.stompClient.send(`/app/trade/${symbol.symbolId}`, {}, (""));
     });
@@ -70,6 +71,7 @@ export class BuysellComponent implements OnInit {
     this.order.symbol = symbol;
 
     this.order.userId = "U_004";
+    console.log(this.order);
 
     this.orderService.buysell(this.order).subscribe(order => {
       this.orderService.sendSelectedOrder(order);
@@ -90,6 +92,10 @@ export class BuysellComponent implements OnInit {
     });
   }
 
+  showMatchedPrice(): void{
+    this.showMatched = !this.showMatched;
+  }
+
   initializeWebSocketConnection(symbolId: number): any {
     console.log('connected to ws ...');
 
@@ -105,6 +111,9 @@ export class BuysellComponent implements OnInit {
       copyStompClient.subscribe(`/topic/trade/${symbolId}`, (daily) => {
         that.daily = that.utils.convertToDailyBidAsk(daily.body);
         console.log(that.daily);
+      });
+      copyStompClient.subscribe(`/topic/trade/${symbolId}/future`, (matched) => {
+        that.matchedPrice = matched.body;
       });
       copyStompClient.send(`/app/trade/${symbolId}`, {}, (""));
     }, (err) => {
