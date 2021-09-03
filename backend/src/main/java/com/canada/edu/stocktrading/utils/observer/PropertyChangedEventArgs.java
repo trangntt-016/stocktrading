3dc9943a0420e2f6ea0ec02f1cbf3250b3faa3db
trangntt-016 <trangntt.016@gmail.com>
2021-09-03 16:49:02 -0400

    refactor redundant codes PropertyChangedEventArgs

--- backend/src/main/java/com/canada/edu/stocktrading/utils/observer/PropertyChangedEventArgs.java
+++ backend/src/main/java/com/canada/edu/stocktrading/utils/observer/PropertyChangedEventArgs.java
@@ -1,13 +1,10 @@
 package com.canada.edu.stocktrading.utils.observer;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class PropertyChangedEventArgs<T>{
     public T source;
     public String userId;
     public String symbol;
-
-    PropertyChangedEventArgs(T source, String userId, String symbol) {
-        this.source = source;
-        this.userId = userId;
-        this.symbol = symbol;
-    }
 }
