aadc8e00f8e3efcb360f59c21005005e21e30ec0
trangntt-016 <trangntt.016@gmail.com>
2021-09-04 11:24:43 -0400

    responsive auth

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { SymbolService } from '../service/symbol.service';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   user: any = null;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    if(this.auth.readToken() !== null){
+      this.user = this.auth.readToken().userId;
+    }
+  }
+
+  logout(): void{
+    this.auth.logout();
+  }
 
 }
